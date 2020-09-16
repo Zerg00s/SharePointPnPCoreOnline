2282ec9fdec3fd4251379355b3a4804b8199c739
Denis Molodtsov <denis-ne@yandex.ru>
2020-09-16 12:37:08 -0400

    removed formatting

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ namespace PnPAuthenticationDemo
             string siteUrl = "https://cleverpointdev.sharepoint.com/sites/GradyTeam";
             var authManager = new AuthenticationManager();
             var ctx =  authManager.GetWebLoginClientContext(siteUrl, null);
-            Console.WriteLine($"Success! Connected to SharePoint Online");
+            Console.WriteLine("Success! Connected to SharePoint Online");
             Web web = ctx.Web;
             ctx.Load(web, w => w.Title);
             ctx.ExecuteQuery();            
-            Console.WriteLine($"Web site title is '{web.Title}'");
+            Console.WriteLine("Web site title is " +  web.Title");
         }
     }
 }
