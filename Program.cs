450214f72b127b61819bae620d35a0b8b4426ffe
Denis Molodtsov <denis-ne@yandex.ru>
2020-09-16 12:23:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+
+namespace PnPAuthenticationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string siteUrl = "https://cleverpointdev.sharepoint.com/sites/GradyTeam";
+            var authManager = new AuthenticationManager();
+            var ctx =  authManager.GetWebLoginClientContext(siteUrl, null);
+            Console.WriteLine($"Success! Connected to SharePoint Online");
+            Web web = ctx.Web;
+            ctx.Load(web, w => w.Title);
+            ctx.ExecuteQuery();            
+            Console.WriteLine($"Web site title is '{web.Title}'");
+        }
+    }
+}
